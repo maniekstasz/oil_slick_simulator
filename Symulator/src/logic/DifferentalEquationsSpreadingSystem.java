99585e25dc9a6903cfcd5516005c6b9904c523e3
maciekg <maciek.glug@uj.edu.pl>
2013-12-06 16:42:01 +0100

    Test

--- Symulator/src/logic/DifferentalEquationsSpreadingSystem.java
+++ Symulator/src/logic/DifferentalEquationsSpreadingSystem.java
@@ -24,6 +24,7 @@ public class DifferentalEquationsSpreadingSystem implements SpreadingSystem {
 	private float previousVolume;
 	private float contentOfWater;
 	private float previouscontentOfWater;
+	
 
 	private float actualArea;
 
